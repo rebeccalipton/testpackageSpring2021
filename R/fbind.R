4ebf8d8ca6dfdcf80cee532c2a5dcf918c8e2cb5
Rebecca Lipton <rebeccarl013@gmail.com>
2021-02-19 11:00:10 -0500

    admin updates

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,13 @@
+#' Concatenate levels of two factors
+#' Title
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return
+#' @export fbind
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
